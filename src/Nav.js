fbe5b07551794d7b1ecfbf9e2b265f4b5fabea23
hediismail <hedi.ismail.tr.1990@gmail.com>
2021-05-31 13:56:06 +0100

    netflix clone

---
+++ src/Nav.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import './Nav.css'
+
+function Nav() {
+  const [show, handleShow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true)
+      } else handleShow(false)
+    })
+    return () => {
+      window.removeEventListener('scroll')
+    }
+  }, [])
+
+  return (
+    <div>
+      <div className={`nav ${show && 'nav_black'}`}>
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/6/67/NewNetflixLogo.png"
+          alt="Netflix Logo"
+        />
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="Netflix Logo"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Nav
